cd609ec592da301b79f799a245f310bc17e8b5af
Ashjan Aljbreen <158529212+Ashmoh00@users.noreply.github.com>
2025-07-05 23:39:14 +0300

    Update generate-report.ps1

--- generate-report.ps1
+++ generate-report.ps1
@@ -3,7 +3,7 @@
 # ============================================
 
 # تأكد إن الملف في نفس المسار أو عدّل المسار لو حاب
-$csvFile = "./AdventureWorks Sales Data 2020.csv"
+$csvFile = "DevOpsDemo/AdventureWorks Sales Data 2020.csv"
 if (-Not (Test-Path $csvFile)) {
     Write-Host "❌ CSV file not found at $csvFile"
     exit 1
